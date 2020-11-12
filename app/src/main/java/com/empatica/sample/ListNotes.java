dc31ea52f6d2a572c52d6d0374790b7693b22602
Robbe <robbevh18@gmail.com>
2020-11-12 13:24:20 +0100

    Robbe Nodes

--- app/src/main/java/com/empatica/sample/ListNotes.java
+++ app/src/main/java/com/empatica/sample/ListNotes.java
@@ -2,17 +2,24 @@ package com.empatica.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class ListNotes extends AppCompatActivity {
     //initialise variable
     EditText editText;
+    Button btAdd,btReset;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listnotes);
+
+        //assign variable
+        editText =
 
     }
 }
