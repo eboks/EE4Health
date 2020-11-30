774c1f9ac953439232d656cd5c64cfde079382f1
JunChatani <chatani.jun@gmail.com>
2020-11-30 16:34:24 +0100

    exmpatica

--- app/src/main/java/com/empatica/sample/fragments/SettingsFragment.java
+++ app/src/main/java/com/empatica/sample/fragments/SettingsFragment.java
@@ -1,23 +1,367 @@
 package com.empatica.sample.fragments;
 
-
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import com.empatica.empalink.ConnectionNotAllowedException;
+import com.empatica.empalink.EmpaDeviceManager;
+import com.empatica.empalink.EmpaticaDevice;
+import com.empatica.empalink.config.EmpaSensorStatus;
+import com.empatica.empalink.config.EmpaSensorType;
+import com.empatica.empalink.config.EmpaStatus;
+import com.empatica.empalink.delegate.EmpaDataDelegate;
+import com.empatica.empalink.delegate.EmpaStatusDelegate;
 import com.empatica.sample.R;
+import com.empatica.sample.activities.MainActivity;
+import com.empatica.sample.adapter.ViewPageAdapter;
+import com.empatica.sample.models.Student;
+import com.google.android.material.tabs.TabLayout;
 
-public class SettingsFragment extends Fragment {
+import static android.content.ContentValues.TAG;
+
+public class SettingsFragment  extends Fragment implements EmpaDataDelegate, EmpaStatusDelegate {
+
+    private View studentOverviewView;
+    private static final int REQUEST_ENABLE_BT = 1;
+
+    private static final int REQUEST_PERMISSION_ACCESS_COARSE_LOCATION = 1;
+
+    private static final String EMPATICA_API_KEY = "574b7f3985804897b373d51a3719229a"; // TODO insert your API Key here
+
+    private EmpaDeviceManager deviceManager = null;
+
+
+
+    private TextView accel_xLabel;
+    private TextView accel_yLabel;
+    private TextView accel_zLabel;
+    private TextView bvpLabel;
+    private TextView edaLabel;
+    private TextView ibiLabel;
+    private TextView temperatureLabel;
+    private TextView batteryLabel;
+    private TextView statusLabel;
+    private TextView deviceNameLabel;
+    private LinearLayout dataCnt;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+
+        studentOverviewView = inflater.inflate(R.layout.fragment_settings, container, false);
+        initVar(studentOverviewView);
+
+        final Button disconnectButton = studentOverviewView.findViewById(R.id.disconnectButton);
+        disconnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (deviceManager != null) {
+                    deviceManager.disconnect();
+                }
+            }
+        });
+
+        initEmpaticaDeviceManager();
+
+
+        return studentOverviewView;
+
     }
+
+    private void initEmpaticaDeviceManager() {
+        // Android 6 (API level 23) now require ACCESS_COARSE_LOCATION permission to use BLE
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[] { Manifest.permission.ACCESS_COARSE_LOCATION }, REQUEST_PERMISSION_ACCESS_COARSE_LOCATION);
+        } else {
+
+            if (TextUtils.isEmpty(EMPATICA_API_KEY)) {
+                new AlertDialog.Builder(getContext())
+                        .setTitle("Warning")
+                        .setMessage("Please insert your API KEY")
+                        .setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // without permission exit is the only way
+                                //finish();
+                            }
+                        })
+                        .show();
+                return;
+            }
+
+            // Create a new EmpaDeviceManager. MainActivity is both its data and status delegate.
+            deviceManager = new EmpaDeviceManager(getContext(), this, this);
+
+            // Initialize the Device Manager using your API key. You need to have Internet access at this point.
+            deviceManager.authenticateWithAPIKey(EMPATICA_API_KEY);
+        }
+    }
+
+
+
+    public  void initVar(View view){
+        // Initialize vars that reference UI components
+        statusLabel = (TextView) view.findViewById(R.id.status);
+
+        dataCnt = (LinearLayout) view.findViewById(R.id.dataArea);
+
+        accel_xLabel = (TextView) view.findViewById(R.id.accel_x);
+
+        accel_yLabel = (TextView) view.findViewById(R.id.accel_y);
+
+        accel_zLabel = (TextView) view.findViewById(R.id.accel_z);
+
+        bvpLabel = (TextView) view.findViewById(R.id.bvp);
+
+        edaLabel = (TextView) view.findViewById(R.id.eda);
+
+        ibiLabel = (TextView) view.findViewById(R.id.ibi);
+
+        temperatureLabel = (TextView) view.findViewById(R.id.temperature);
+
+        batteryLabel = (TextView) view.findViewById(R.id.battery);
+
+        deviceNameLabel = (TextView) view.findViewById(R.id.deviceName);
+
+
+    }
+
+    // Update a label with some text, making sure this is run in the UI thread
+    private void updateLabel(final TextView label, final String text) {
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                label.setText(text);
+            }
+        });
+    }
+
+
+
+    @Override
+    public void didReceiveAcceleration(int x, int y, int z, double timestamp) {
+        updateLabel(accel_xLabel, "" + x);
+        updateLabel(accel_yLabel, "" + y);
+        updateLabel(accel_zLabel, "" + z);
+    }
+
+    @Override
+    public void didReceiveBVP(float bvp, double timestamp) {
+        updateLabel(bvpLabel, "" + bvp);
+    }
+
+    @Override
+    public void didReceiveBatteryLevel(float battery, double timestamp) {
+        updateLabel(batteryLabel, String.format("%.0f %%", battery * 100));
+    }
+
+    @Override
+    public void didReceiveGSR(float gsr, double timestamp) {
+        updateLabel(edaLabel, "" + gsr);
+    }
+
+    @Override
+    public void didReceiveIBI(float ibi, double timestamp) {
+        updateLabel(ibiLabel, "" + ibi);
+    }
+
+    @Override
+    public void didReceiveTemperature(float temp, double timestamp) {
+        updateLabel(temperatureLabel, "" + temp);
+    }
+
+
+    @Override
+    public void didReceiveTag(double timestamp) {
+
+    }
+
+
+    @Override
+    public void didEstablishConnection() {
+
+    }
+
+
+    @Override
+    public void didDiscoverDevice(EmpaticaDevice bluetoothDevice, String deviceName, int rssi, boolean allowed) {
+        // Check if the discovered device can be used with your API key. If allowed is always false,
+        // the device is not linked with your API key. Please check your developer area at
+        // https://www.empatica.com/connect/developer.php
+        if (allowed) {
+            // Stop scanning. The first allowed device will do.
+            deviceManager.stopScanning();
+            try {
+                // Connect to the device
+                deviceManager.connectDevice(bluetoothDevice);
+                updateLabel(deviceNameLabel, "To: " + deviceName);
+            } catch (ConnectionNotAllowedException e) {
+                // This should happen only if you try to connect when allowed == false.
+                Toast.makeText(getContext(), "Sorry, you can't connect to this device", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    @Override
+    public void didFailedScanning(int errorCode) {
+
+    }
+
+    @Override
+    public void didRequestEnableBluetooth() {
+
+    }
+
+    @Override
+    public void bluetoothStateChanged() {
+
+    }
+
+    @Override
+    public void didUpdateOnWristStatus(@EmpaSensorStatus final int status) {
+
+        getActivity().runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                if (status == EmpaSensorStatus.ON_WRIST) {
+
+                    ((TextView) getView().findViewById(R.id.wrist_status_label)).setText("ON WRIST");
+                }
+                else {
+
+                    ((TextView) getView().findViewById(R.id.wrist_status_label)).setText("NOT ON WRIST");
+                }
+            }
+        });
+    }
+    @Override
+    public void didUpdateSensorStatus(@EmpaSensorStatus int status, EmpaSensorType type) {
+
+        didUpdateOnWristStatus(status);
+    }
+
+    @Override
+    public void didUpdateStatus(EmpaStatus status) {
+        // Update the UI
+        updateLabel(statusLabel, status.name());
+
+        // The device manager is ready for use
+        if (status == EmpaStatus.READY) {
+            updateLabel(statusLabel, status.name() + " - Turn on your device");
+            // Start scanning
+            deviceManager.startScanning();
+            // The device manager has established a connection
+
+            hide();
+
+        } else if (status == EmpaStatus.CONNECTED) {
+
+            show();
+            // The device manager disconnected from a device
+        } else if (status == EmpaStatus.DISCONNECTED) {
+
+            updateLabel(deviceNameLabel, "");
+
+            hide();
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_ACCESS_COARSE_LOCATION:
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission was granted, yay!
+                    initEmpaticaDeviceManager();
+                } else {
+                    // Permission denied, boo!
+                    final boolean needRationale = ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION);
+                    new AlertDialog.Builder(getContext())
+                            .setTitle("Permission required")
+                            .setMessage("Without this permission bluetooth low energy devices cannot be found, allow it in order to connect to the device.")
+                            .setPositiveButton("Retry", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // try again
+                                    if (needRationale) {
+                                        // the "never ask again" flash is not set, try again with permission request
+                                        initEmpaticaDeviceManager();
+                                    }
+                                    /*else {
+                                        // the "never ask again" flag is set so the permission requests is disabled, try open app settings to enable the permission
+                                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                        Uri uri = Uri.fromParts("package", getPackageName(), null);
+                                        intent.setData(uri);
+                                        startActivity(intent);
+                                    }*/
+                                }
+                            })
+                            .setNegativeButton("Exit application", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // without permission exit is the only way
+                                 //   finish();
+                                }
+                            })
+                            .show();
+                }
+                break;
+        }
+    }
+
+    void show() {
+
+        getActivity().runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                dataCnt.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    void hide() {
+
+        getActivity().runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+
+                dataCnt.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+
+
+
+
 }
+
 
